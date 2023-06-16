7278a977b0a6236791053cb28427b92c54f126a8
jiafeng <jiafeng@git.com>
2023-06-16 19:31:37 +0800

    udate

--- build/Products/VoodooPS2Controller.build/Debug/VoodooPS2Trackpad.build/Script-CEC0864F243E618200F5B701.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f "${PROJECT_DIR}/VoodooInput/Debug/VoodooInput.kext/Contents/Resources/VoodooInputMultitouch/VoodooInputEvent.h" ]; then
-  cd "${PROJECT_DIR}"
-  rm -rf VoodooInput
-  src=$(/usr/bin/curl -Lfs https://raw.githubusercontent.com/acidanthera/VoodooInput/master/VoodooInput/Scripts/bootstrap.sh) && eval "$src" || exit 1
-fi
-
